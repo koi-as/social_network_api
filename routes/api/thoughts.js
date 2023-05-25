afcf460d8cb9fe684855f6a65441f8e7a07c7070
Koi-AS <lightningblade.901@gmail.com>
2023-05-25 15:56:30 -0400

    Mapped out the CRUD operations for api routes

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -1,2 +1,14 @@
-// /
-// /:thoughtId/reactions
+const router = require('express').Router();
+
+// localhost:4001/api/thoughts/
+// GET all thoughts
+// GET a single thought by _id
+// POST create a new thought
+// PUT update a thought by _id
+// DELETE a thought by _id
+
+// localhost:4001/api/thoughts/:thoughtId/reactions
+// POST a reaction to thoughts reaction array
+// DELETE a reaction from thoughts reaction array
+
+module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,2 +1,14 @@
-// /
-// /:userId/friends/:friendId
+const router = require('express').Router();
+
+// localhost:4001/api/users/
+// GET all users
+// GET user by _id
+// POST create new user
+// PUT update user by _id
+// DELETE user by _id
+
+// localhost:4001/api/users/:userId/friends/:friendId
+// POST a new friend to user friend list
+// DELETE a friend from user friend list
+
+module.exports = router;
