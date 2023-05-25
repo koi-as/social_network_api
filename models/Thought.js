07c56304fe1e4b60ef5131448093b895eb3733ab
Koi-AS <lightningblade.901@gmail.com>
2023-05-25 15:47:43 -0400

    updated the Thought.js model

--- models/Thought.js
+++ models/Thought.js
@@ -1,20 +1,49 @@
-// * `thoughtText`
-//   * String
-//   * Required
-//   * Must be between 1 and 280 characters
+const mongoose = require('mongoose');
 
-// * `createdAt`
-//   * Date
-//   * Set default value to the current timestamp
-//   * Use a getter method to format the timestamp on query
+// **Schema Settings**:
 
-// * `username` (The user that created this thought)
-//   * String
-//   * Required
-
-// * `reactions` (These are like replies)
-//   * Array of nested documents created with the `reactionSchema`
+// This will not be a model, but rather will be used as the `reaction` field's subdocument schema in the `Thought` model.
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        // Use mongoose's ObjectId data type
+        // default: set to a new ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        // date
+        // set default to current timestamp
+        // use a getter method to format timestamp
+    }
+})
 
 // **Schema Settings**:
 
 // Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 100
+    },
+    createdAt: {
+        // date
+        // default value to current timestamp
+        // getter method to format timestamp
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [
+        // array of nested reactions created with reactionSchema
+    ]
+})
