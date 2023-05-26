65b3a58748706847a12625b7fa194520a300671c
Koi-AS <lightningblade.901@gmail.com>
2023-05-26 17:39:29 -0400

    Added get and post requests for users route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,49 +3,62 @@ const { User } = require('../models')
 module.exports = {
     async getUsers (req, res) {
         try {
-            
+            // GET all users 
+            const userData = await User.find();
+
+            res.status(200).json(userData); 
         } catch (err) {
             res.status(500).json(err);
         }
     },
     async getOneUser (req, res) {
         try {
-            
+            // GET user by _id
+            const userData = await User.findOne({ _id: req.params.userId})
+
+            if (!userdata) {
+                res.status(400).json('User not found!');
+            };
+
+            res.status(200).json(userData);
         } catch (err) {
             res.status(500).json(err);
         }
     },
     async createUser (req, res) {
         try {
-            
+            // POST create new user
+            const userData = await User.create(req.body)
+
+            res.status(200).json(userData)
         } catch (err) {
             res.status(500).json(err);
         }
     },
     async updateUser (req, res) {
         try {
-            
+            // PUT update user by _id
         } catch (err) {
             res.status(500).json(err);
         }
     },
     async deleteUser (req, res) {
         try {
-            
+            // DELETE user by _id
         } catch (err) {
             res.status(500).json(err);
         }
     },
     async newFriend (req, res) {
         try {
-            
+            // POST a new friend to user friend list
         } catch (err) {
             res.status(500).json(err);
         }
     },
     async removeFriend (req, res) {
         try {
-            
+            // DELETE a friend from user friend list
         } catch (err) {
             res.status(500).json(err);
         }
--- routes/api/users.js
+++ routes/api/users.js
@@ -13,23 +13,16 @@ const {
 router.route('/')
     .get(getUsers)
     .post(createUser)
-// GET all users
-// POST create new user
 
 // localhost:4001/api/users/:userId/
 router.route('/:userId')
     .get(getOneUser)
     .put(updateUser)
     .delete(deleteUser)
-// GET user by _id
-// PUT update user by _id
-// DELETE user by _id
 
 // localhost:4001/api/users/:userId/friends/:friendId/
 router.route('/:userId/friends/:friendId')
     .post(newFriend)
     .delete(removeFriend)
-// POST a new friend to user friend list
-// DELETE a friend from user friend list
 
 module.exports = router;
