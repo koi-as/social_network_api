0b2fce197674e77eea04fa57c25bad45239c0838
Koi-AS <lightningblade.901@gmail.com>
2023-05-26 17:38:46 -0400

    Required routes for server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express'); // require express.js
 const db = require('./config/connection.js'); // require the mongoose connection
+
+const routes = require('./routes')
 // create the app and port
 const app = express();
 const PORT = 4001 || process.env.PORT;
 // require for making post requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
