668d6fbf892358928e5efc5b49b62426e44aabce
Koi-AS <lightningblade.901@gmail.com>
2023-05-26 17:10:50 -0400

    Required controllers in both routes

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -1,25 +1,34 @@
 const router = require('express').Router();
+const {
+    getThoughts,
+    getOneThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtsController.js');
 
 // localhost:4001/api/thoughts/
 router.route('/')
-    .get()
-    .post()
+    .get(getThoughts)
+    .post(createThought)
 // GET all thoughts
 // POST create a new thought
 
 // localhost:4001/thoughts/:thoughtId/
 router.route('/:thoughtId')
-    .get()
-    .put()
-    .delete()
+    .get(getOneThought)
+    .put(updateThought)
+    .delete(deleteThought)
 // GET a single thought by _id
 // PUT update a thought by _id
 // DELETE a thought by _id
 
 // localhost:4001/api/thoughts/:thoughtId/reactions/
 router.route('/:thoughtId/reactions')
-    .post()
-    .delete()
+    .post(addReaction)
+    .delete(deleteReaction)
 // POST a reaction to thoughts reaction array
 // DELETE a reaction from thoughts reaction array
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,25 +1,34 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    newFriend,
+    removeFriend
+} = require('../../controllers/usersController.js');
 
 // localhost:4001/api/users/
 router.route('/')
-    .get()
-    .post()
+    .get(getUsers)
+    .post(createUser)
 // GET all users
 // POST create new user
 
 // localhost:4001/api/users/:userId/
 router.route('/:userId')
-    .get()
-    .put()
-    .delete()
+    .get(getOneUser)
+    .put(updateUser)
+    .delete(deleteUser)
 // GET user by _id
 // PUT update user by _id
 // DELETE user by _id
 
 // localhost:4001/api/users/:userId/friends/:friendId/
 router.route('/:userId/friends/:friendId')
-    .post()
-    .delete()
+    .post(newFriend)
+    .delete(removeFriend)
 // POST a new friend to user friend list
 // DELETE a friend from user friend list
 
