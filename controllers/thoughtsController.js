3e56b8eafd4d281955fc2758dfe15260b3233bf3
Koi-AS <lightningblade.901@gmail.com>
2023-05-26 17:05:29 -0400

    created controllers for the routes

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,53 @@
+const { Thought } = require('../models');
+
+module.exports = {
+    async getThoughts (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getOneThought (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createThought (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateThought (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteThought (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async addReaction (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteReaction (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
---
+++ controllers/usersController.js
@@ -0,0 +1,53 @@
+const { User } = require('../models')
+
+module.exports = {
+    async getUsers (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getOneUser (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createUser (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateUser (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteUser (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async newFriend (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async removeFriend (req, res) {
+        try {
+            
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
